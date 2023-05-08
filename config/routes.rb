3f046eeb1ef9b4d257c0e09c813ece743e00dde3
NetesaRoman <netesroman8@gmail.com>
2023-05-08 14:32:15 +0300

    + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/show'
-  get 'reviews/new'
-  get 'reviews/index'
-  get 'reviews/edit'
-  get 'rating/show'
-  get 'rating/new'
-  get 'rating/index'
-  get 'rating/edit'
-  get 'movie_shorts/show'
-  get 'movie_shorts/new'
-  get 'movie_shorts/index'
-  get 'movie_shorts/edit'
-  get 'movies/show'
-  get 'movies/new'
-  get 'movies/index'
-  get 'movies/edit'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/index'
-  get 'categories/edit'
-  get 'actors/show'
-  get 'actors/new'
-  get 'actors/index'
-  get 'actors/edit'
-  get 'genres/show'
-  get 'genres/new'
-  get 'genres/index'
-  get 'genres/edit'
+
 
 
   devise_for :users
@@ -34,7 +7,14 @@ Rails.application.routes.draw do
   get "ping", to: "debug#index"
 
   resources :main
-
+  resources :actors
+  resources :categories
+  resources :genres
+  resources :movie_shorts
+  resources :movies
+  resources :rating_stars
+  resources :rating
+  resources :reviews
 
   root "main#index"
 end
