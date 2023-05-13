5caa9a81d9cb9687fcb37e8db43a110de993bb47
NetesaRoman <netesroman8@gmail.com>
2023-05-13 15:51:04 +0300

    Cleanup + admin role + movies show instead of shorts

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -2,4 +2,10 @@ class DebugController < ApplicationController
   before_action :authenticate_user!
   def index
   end
+
+  def admin
+    unless current_user.admin?
+      redirect_to 'ping', allow_other_host: true
+    end
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,11 +4,7 @@ class MoviesController < ApplicationController
   def index
 
     @q = Movie.where(draft: true).ransack(params[:q])
-    @movies = @q.result(distinct: true)
-
-
-
-    @movie_shorts = MovieShort.where(movie_id: @movies.pluck(:id)).paginate(page: params[:page], per_page: 5)
+    @movies = @q.result(distinct: true).paginate(page: params[:page], per_page: 5)
 
   end
 
@@ -17,22 +13,38 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
+
+      @movie = Movie.find(params[:id])
+
+
   end
 
   def update
-    @movie = Movie.find(params[:id])
+    if current_user.admin?
+      @movie = Movie.find(params[:id])
 
-    @movie.update(movie_params)
+      @movie.update(movie_params)
 
+
+    end
     redirect_to movie_path(@movie)
   end
 
   def new
-    @movie = Movie.new
+
+    unless current_user.admin
+      redirect_to movies_path, allow_other_host: true
+    end
+
+      @movie = Movie.new
+
+
   end
 
   def create
+
+
+
     @movie = Movie.new(movie_params)
     if @movie.save
       # Загрузка файла на сервер
@@ -40,7 +52,6 @@ class MoviesController < ApplicationController
       file_path = Rails.root.join('public', 'posters', uploaded_file.original_filename)
       File.open(file_path, 'wb') do |file|
         file.write(uploaded_file.read)
-
       end
 
       # Сохранение пути к файлу в базе данных
@@ -56,27 +67,31 @@ class MoviesController < ApplicationController
       actor_ids.each do |actor_id|
         @movie.movie_actors.create(actor_id: actor_id)
       end
-
       director_ids = params[:movie][:director_ids] || []
       director_ids.each do |director_id|
         @movie.movie_directors.create(director_id: director_id)
       end
-    end
-    redirect_to movies_path, allow_other_host: true
 
+      redirect_to movies_path, allow_other_host: true
+    else
+      render :new
+    end
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
+    if current_user.admin?
+      @movie = Movie.find(params[:id])
+      @movie.destroy
 
-    redirect_to movies_path, allow_other_host: true
+      redirect_to movies_path, allow_other_host: true
+    end
+
   end
 
   private
 
   def movie_params
     params.require(:movie).permit(:title, :description, :poster, :tagline, :year, :country, :world_premier,
-                                  :budget, :fees_in_usa, :fees_in_world, :category_id, :url, :draft)
+                                  :budget, :fees_in_usa, :fees_in_world, :category_id, :url, :draft,  actor_ids: [], director_ids: [])
   end
 end
